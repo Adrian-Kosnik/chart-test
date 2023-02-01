e168474e96d664a9a5f834a8d5d4433e22a7a846
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-02-01 21:33:32 +0000

    finished function

---
+++ script.js
@@ -0,0 +1,218 @@
+let responseArr = {
+    "status": "success",
+    "data": {
+      "change": "1.95",
+      "history": [
+        { "price": "23027.597965987155", "timestamp": 1675274400 },
+        { "price": "23039.975183379458", "timestamp": 1675270800 },
+        { "price": "23039.524527287216", "timestamp": 1675267200 },
+        { "price": "23101.96268778202", "timestamp": 1675263600 },
+        { "price": "23098.892295359983", "timestamp": 1675260000 },
+        { "price": "23083.91529500675", "timestamp": 1675256400 },
+        { "price": "23070.48269472378", "timestamp": 1675252800 },
+        { "price": "23013.62197804494", "timestamp": 1675249200 },
+        { "price": "23040.490853376017", "timestamp": 1675245600 },
+        { "price": "23032.23506658196", "timestamp": 1675242000 },
+        { "price": "23090.021445717997", "timestamp": 1675238400 },
+        { "price": "23097.38305121559", "timestamp": 1675234800 },
+        { "price": "23131.853617097637", "timestamp": 1675231200 },
+        { "price": "23150.32771057754", "timestamp": 1675227600 },
+        { "price": "23143.81679007109", "timestamp": 1675224000 },
+        { "price": "23110.85440358268", "timestamp": 1675220400 },
+        { "price": "23129.831335532", "timestamp": 1675216800 },
+        { "price": "23113.993542782424", "timestamp": 1675213200 },
+        { "price": "23106.367763276987", "timestamp": 1675209600 },
+        { "price": "23139.08898544325", "timestamp": 1675206000 },
+        { "price": "22967.15074897543", "timestamp": 1675202400 },
+        { "price": "23112.014962776397", "timestamp": 1675198800 },
+        { "price": "23145.76665995167", "timestamp": 1675195200 },
+        { "price": "23169.569334723783", "timestamp": 1675191600 },
+        { "price": "23142.682734511265", "timestamp": 1675188000 },
+        { "price": "23103.220483238085", "timestamp": 1675184400 },
+        { "price": "23120.852772229893", "timestamp": 1675180800 },
+        { "price": "23093.607123871916", "timestamp": 1675177200 },
+        { "price": "23044.62963188957", "timestamp": 1675173600 },
+        { "price": "22891.89362662157", "timestamp": 1675170000 },
+        { "price": "22872.983192462063", "timestamp": 1675166400 },
+        { "price": "22869.3348588211", "timestamp": 1675162800 },
+        { "price": "22919.305578796866", "timestamp": 1675159200 },
+        { "price": "22919.355441779524", "timestamp": 1675155600 },
+        { "price": "22938.475526777267", "timestamp": 1675152000 },
+        { "price": "22824.50766089288", "timestamp": 1675148400 },
+        { "price": "22794.70070369636", "timestamp": 1675144800 },
+        { "price": "22871.51883596166", "timestamp": 1675141200 },
+        { "price": "22847.389026661294", "timestamp": 1675137600 },
+        { "price": "22870.003045803503", "timestamp": 1675134000 },
+        { "price": "22828.781377992294", "timestamp": 1675130400 },
+        { "price": "22813.139213559225", "timestamp": 1675126800 },
+        { "price": "22813.83063540092", "timestamp": 1675123200 },
+        { "price": "22787.658352141236", "timestamp": 1675119600 },
+        { "price": "22778.03103026047", "timestamp": 1675116000 },
+        { "price": "22742.209585340664", "timestamp": 1675112400 },
+        { "price": "22884.499838048643", "timestamp": 1675108800 },
+        { "price": "23172.59759576832", "timestamp": 1675105200 },
+        { "price": "23146.9732414299", "timestamp": 1675101600 },
+        { "price": "23212.19191915108", "timestamp": 1675098000 },
+        { "price": "23201.466578485746", "timestamp": 1675094400 },
+        { "price": "23257.09644323079", "timestamp": 1675090800 },
+        { "price": "23099.96432186062", "timestamp": 1675087200 },
+        { "price": "23154.187305263964", "timestamp": 1675083600 },
+        { "price": "23182.64542688087", "timestamp": 1675080000 },
+        { "price": "23315.288846382544", "timestamp": 1675076400 },
+        { "price": "23319.5987005371", "timestamp": 1675072800 },
+        { "price": "23645.296414725686", "timestamp": 1675069200 },
+        { "price": "23675.628107753826", "timestamp": 1675065600 },
+        { "price": "23725.508410371414", "timestamp": 1675062000 },
+        { "price": "23735.889592387837", "timestamp": 1675058400 },
+        { "price": "23730.45558490265", "timestamp": 1675054800 },
+        { "price": "23703.86282595141", "timestamp": 1675051200 },
+        { "price": "23672.31240596781", "timestamp": 1675047600 },
+        { "price": "23700.5450784897", "timestamp": 1675044000 },
+        { "price": "23746.709748175257", "timestamp": 1675040400 },
+        { "price": "23770.707406282785", "timestamp": 1675036800 },
+        { "price": "23801.306248587574", "timestamp": 1675033200 },
+        { "price": "23791.2151430281", "timestamp": 1675029600 },
+        { "price": "23761.55040742835", "timestamp": 1675026000 },
+        { "price": "23838.005944500906", "timestamp": 1675022400 },
+        { "price": "23645.87410581674", "timestamp": 1675018800 },
+        { "price": "23585.819053108116", "timestamp": 1675015200 },
+        { "price": "23559.649772411984", "timestamp": 1675011600 },
+        { "price": "23517.277150030215", "timestamp": 1675008000 },
+        { "price": "23512.92048880204", "timestamp": 1675004400 },
+        { "price": "23532.589842052035", "timestamp": 1675000800 },
+        { "price": "23454.733880710475", "timestamp": 1674997200 },
+        { "price": "23406.3893262169", "timestamp": 1674993600 },
+        { "price": "23402.43950112972", "timestamp": 1674990000 },
+        { "price": "23278.379680642927", "timestamp": 1674986400 },
+        { "price": "23206.356283189194", "timestamp": 1674982800 },
+        { "price": "23224.230509094505", "timestamp": 1674979200 },
+        { "price": "23222.515866594957", "timestamp": 1674975600 },
+        { "price": "23245.84557757753", "timestamp": 1674972000 },
+        { "price": "23227.87056077749", "timestamp": 1674968400 },
+        { "price": "23192.36470607431", "timestamp": 1674964800 },
+        { "price": "23207.52672044796", "timestamp": 1674961200 },
+        { "price": "23246.769152386387", "timestamp": 1674957600 },
+        { "price": "23099.8075336419", "timestamp": 1674954000 },
+        { "price": "23051.52688817389", "timestamp": 1674950400 },
+        { "price": "23009.975981923177", "timestamp": 1674946800 },
+        { "price": "23047.29824158491", "timestamp": 1674943200 },
+        { "price": "23028.524915379734", "timestamp": 1674939600 },
+        { "price": "23055.074749198957", "timestamp": 1674936000 },
+        { "price": "23039.1799819144", "timestamp": 1674932400 },
+        { "price": "23041.923640240824", "timestamp": 1674928800 },
+        { "price": "23038.194969499797", "timestamp": 1674925200 },
+        { "price": "23015.901485850827", "timestamp": 1674921600 },
+        { "price": "22989.167767134946", "timestamp": 1674918000 },
+        { "price": "22999.64513883288", "timestamp": 1674914400 },
+        { "price": "22993.720595163322", "timestamp": 1674910800 },
+        { "price": "23015.140698170544", "timestamp": 1674907200 },
+        { "price": "23021.833291472783", "timestamp": 1674903600 },
+        { "price": "23020.782091205354", "timestamp": 1674900000 },
+        { "price": "23020.614895543826", "timestamp": 1674896400 },
+        { "price": "23029.153665182912", "timestamp": 1674892800 },
+        { "price": "23070.668175607996", "timestamp": 1674889200 },
+        { "price": "23094.84932078649", "timestamp": 1674885600 },
+        { "price": "23106.670160537833", "timestamp": 1674882000 },
+        { "price": "23114.12350682797", "timestamp": 1674878400 },
+        { "price": "23117.1192782739", "timestamp": 1674874800 },
+        { "price": "23117.656083031325", "timestamp": 1674871200 },
+        { "price": "23139.480969892236", "timestamp": 1674867600 },
+        { "price": "23095.659600448733", "timestamp": 1674864000 },
+        { "price": "23073.199189423034", "timestamp": 1674860400 },
+        { "price": "23141.860166203267", "timestamp": 1674856800 },
+        { "price": "23197.29525398257", "timestamp": 1674853200 },
+        { "price": "23229.294517108727", "timestamp": 1674849600 },
+        { "price": "23189.97630467379", "timestamp": 1674846000 },
+        { "price": "23164.23006299531", "timestamp": 1674842400 },
+        { "price": "23115.460658498338", "timestamp": 1674838800 },
+        { "price": "23024.31651794492", "timestamp": 1674835200 },
+        { "price": "22926.451523894135", "timestamp": 1674831600 },
+        { "price": "22912.885317150092", "timestamp": 1674828000 },
+        { "price": "22962.26177233451", "timestamp": 1674824400 },
+        { "price": "22996.871686313803", "timestamp": 1674820800 },
+        { "price": "22969.54276008727", "timestamp": 1674817200 },
+        { "price": "22983.705047757645", "timestamp": 1674813600 },
+        { "price": "23006.355053877167", "timestamp": 1674810000 },
+        { "price": "23067.802774304164", "timestamp": 1674806400 },
+        { "price": "22990.073448407824", "timestamp": 1674802800 },
+        { "price": "22923.406564995777", "timestamp": 1674799200 },
+        { "price": "22822.580086372935", "timestamp": 1674795600 },
+        { "price": "22802.805190440475", "timestamp": 1674792000 },
+        { "price": "22818.150966355806", "timestamp": 1674788400 },
+        { "price": "22791.74540550116", "timestamp": 1674784800 },
+        { "price": "23017.126834009214", "timestamp": 1674781200 },
+        { "price": "23023.174259010288", "timestamp": 1674777600 },
+        { "price": "23021.07688252775", "timestamp": 1674774000 },
+        { "price": "23082.495310778155", "timestamp": 1674770400 },
+        { "price": "23102.746205150663", "timestamp": 1674766800 },
+        { "price": "23091.82520901604", "timestamp": 1674763200 },
+        { "price": "23067.157105138554", "timestamp": 1674759600 },
+        { "price": "23005.80676339653", "timestamp": 1674756000 },
+        { "price": "23047.418486929622", "timestamp": 1674752400 },
+        { "price": "23037.73202167018", "timestamp": 1674748800 },
+        { "price": "23035.27201342429", "timestamp": 1674745200 },
+        { "price": "23083.58597816636", "timestamp": 1674741600 },
+        { "price": "23093.89834916073", "timestamp": 1674738000 },
+        { "price": "22996.383660994397", "timestamp": 1674734400 },
+        { "price": "22975.626298737086", "timestamp": 1674730800 },
+        { "price": "23002.411115652318", "timestamp": 1674727200 },
+        { "price": "23019.41747452648", "timestamp": 1674723600 },
+        { "price": "23016.334259803614", "timestamp": 1674720000 },
+        { "price": "23123.21097201162", "timestamp": 1674716400 },
+        { "price": "23163.45583008182", "timestamp": 1674712800 },
+        { "price": "23188.493120960935", "timestamp": 1674709200 },
+        { "price": "23164.398051072953", "timestamp": 1674705600 },
+        { "price": "23186.833708628983", "timestamp": 1674702000 },
+        { "price": "23167.27171185685", "timestamp": 1674698400 },
+        { "price": "23181.32038066767", "timestamp": 1674694800 },
+        { "price": "23049.500823109127", "timestamp": 1674691200 },
+        { "price": "22993.376456116035", "timestamp": 1674687600 },
+        { "price": "23411.22666668226", "timestamp": 1674684000 },
+        { "price": "22856.746009452203", "timestamp": 1674680400 },
+        { "price": "22726.462134017856", "timestamp": 1674676800 },
+        { "price": "22700.22694037913", "timestamp": 1674673200 }
+      ]
+    }
+  };
+
+
+// This function needs the response object/json of historic coin prices and the id of a canvas element
+// We need this element where ever we want a chart => <canvas id="{WHATEVER-ID-WE-GIVE-IT}"></canvas>
+// The chart will apper inside that element
+
+function drawGraph(responseArray, canvasId) {
+    // Initialising arrays for dates and coin prices
+    let labelDates = [];
+    let priceDataSets = [];
+
+    for(let i = 0; i < responseArray.data.history.length; i++) {
+        priceDataSets.push(responseArray.data.history[i].price);
+
+        // Converting UTC seconds to normal date - https://java2blog.com/convert-epoch-time-to-date-javascript/
+        let utcSeconds = responseArray.data.history[i].timestamp;
+        let date = new Date(utcSeconds*1000);
+        let resultFormat = date.toISOString().split('T')[0]
+        labelDates.push(resultFormat);
+    };
+
+    // This is what is needed to build the chart
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext('2d');
+
+    // This makes a mad line chart
+    new Chart(context, {
+        type: 'line',
+        data: {
+            labels: labelDates,
+            datasets: [
+                {
+                    label: 'Price in USD',
+                    data: priceDataSets
+                }
+            ]
+        }
+    });
+};
+
+drawGraph(responseArr, 'chartContainer');
+
